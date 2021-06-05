6fd4967e24caf87cff5df4c8cd9d5ca59bad559e
Atsushi Morimoto (74th) <site@74th.tech>
2021-06-05 12:55:52 +0900

    using platform io

--- arduino_uno_platformio_joystick/src/main.cpp
+++ arduino_uno_platformio_joystick/src/main.cpp
@@ -1,9 +1,32 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+const int Y_PIN = A0;
+const int LED_PIN = 2;
+
+void setup()
+{
+    pinMode(Y_PIN, INPUT);
+    pinMode(LED_PIN, OUTPUT);
+    Serial.begin(9600);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+    int y;
+
+    y = analogRead(Y_PIN);
+
+    Serial.print("Y:");
+    Serial.println(y);
+
+    if (y < 300)
+    {
+        digitalWrite(LED_PIN, 0);
+    }
+    else
+    {
+        digitalWrite(LED_PIN, 1);
+    }
+
+    delay(100);
 }
