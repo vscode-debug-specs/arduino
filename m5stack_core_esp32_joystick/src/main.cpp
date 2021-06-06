6591a1a18c90efdf702e703057524aa903ec7079
Atsushi Morimoto (74th) <site@74th.tech>
2021-06-06 10:19:43 +0900

    add esp32

---
+++ m5stack_core_esp32_joystick/src/main.cpp
@@ -0,0 +1,32 @@
+#include <Arduino.h>
+#include <M5Stack.h>
+
+const int LED_PIN = G2;
+int num = 0;
+
+void setup()
+{
+    // put your setup code here, to run once:
+    M5.begin();
+    M5.Power.begin();
+    pinMode(LED_PIN, OUTPUT);
+    Serial.begin(9600);
+}
+
+void loop()
+{
+    if (num % 2 == 0)
+    {
+        M5.Lcd.fillScreen(WHITE);
+        digitalWrite(LED_PIN, HIGH);
+    }
+    else
+    {
+        M5.Lcd.fillScreen(GREEN);
+        digitalWrite(LED_PIN, LOW);
+    }
+    num++;
+    Serial.print("num:");
+    Serial.println(num);
+    delay(1000);
+}
